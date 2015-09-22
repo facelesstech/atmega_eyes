c8a19c32d5061eeb7b8a4af2f9dba8c1f4d32cec
facelessloser <facelessloser@gmail.com>
2015-09-22 21:00:42 +0100

    updated the eyes and added button control to the rgb leds and candle led

--- avr_code/atmega_eyes_final/atmega_eyes_final.c
+++ avr_code/atmega_eyes_final/atmega_eyes_final.c
@@ -7,6 +7,7 @@
 #include <avr/interrupt.h>
 #include <util/atomic.h>
 
+// Pins for the led matrix
 #define CLK_HIGH()  PORTB |= (1<<PB2)
 #define CLK_LOW()   PORTB &= ~(1<<PB2)
 #define CS_HIGH()   PORTB |= (1<<PB1)
@@ -15,35 +16,44 @@
 #define DATA_LOW()  PORTB &= ~(1<<PB0)
 #define INIT_PORT() DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB2)
 
+int buttonPushCounter = 1; // Keeps count of the button pushes
+
 const __flash uint8_t eye[8] = {
+        0b00000000,
         0b00111100,
-        0b01000010,
-        0b10000001,
-        0b10011001,
-        0b10011001,
-        0b10000001,
-        0b01000010,
-        0b00111100};
+        0b01111110,
+        0b01100110,
+        0b01100110,
+        0b01111110,
+        0b00111100,
+        0b0000000};
 
 const __flash uint8_t eye1[8] = {
+        0b00000000,
         0b00111100,
-        0b01000010,
-        0b10000001,
-        0b10110001,
-        0b10110001,
-        0b10000001,
-        0b01000010,
-        0b00111100};
+        0b01111110,
+        0b01001110,
+        0b01001110,
+        0b01111110,
+        0b00111100,
+        0b0000000};
 
 const __flash uint8_t eye2[8] = {
+        0b00000000,
         0b00111100,
-        0b01000010,
-        0b10000001,
-        0b10001101,
-        0b10001101,
-        0b10000001,
-        0b01000010,
-        0b00111100};
+        0b01111110,
+        0b01110010,
+        0b01110010,
+        0b01111110,
+        0b00111100,
+        0b0000000};
+
+void setColor(int red, int green, int blue)
+{
+  OCR2B = 255 - red; // Set PWM rate 0 - 255 RED
+  OCR0B = 255 - green; // Set PWM rate 0 - 255 GREEN
+  OCR0A = 255 - blue; // Set PWM rate 0 - 255 BLUE
+}
 
 void spi_send(uint8_t data)
 {
@@ -131,6 +141,7 @@ void set_pixel(uint8_t r, uint8_t c, uint8_t value)
     }
 }
 
+// Timer amounts
 volatile unsigned long timer1_millis;
 long milliseconds_since1 = 0;
 long milliseconds_since2 = 1000;
@@ -147,23 +158,21 @@ unsigned long millis()
 {
   unsigned long millis_return;
 
-  //ATOMIC_BLOCK(ATOMIC_FORCEON) { // Ensure this cannot be disrupted
   millis_return = timer1_millis;
-   // }
  
   return millis_return;
 }
 
-void flash_led ()
+void eye_display ()
 {
   if (millis() >= milliseconds_since1) {
-    image(eye);
+    image(eye); // Load image
     update_display();
     milliseconds_since1 += waitTime;
     }
 
   if (millis() >= milliseconds_since2) {
-    image(eye1);
+    image(eye1); // Load image
     update_display();
     milliseconds_since2 += waitTime;
     }
@@ -188,8 +197,7 @@ int Released_Confidence_Level = 0; //Measure button release confidence
 
 void button(void)
 {
-    //if (bit_is_clear(PINB, 0))
-    if (bit_is_clear(PIND, 4))
+    if (bit_is_clear(PIND, 4)) // PD4
     {
       Pressed_Confidence_Level ++; //Increase Pressed Conficence
       Released_Confidence_Level = 0; //Reset released button confidence since there is a button press
@@ -198,8 +206,12 @@ void button(void)
         if (Pressed == 0)
         {
           // Do stuff here
-          //PORTB ^= 1 << PINB3; // Toggle LED
-          PORTD ^= 1 << PIND7; // Toggle LED
+          PORTD &= ~_BV(PORTD7); // Candle LED off
+          buttonPushCounter++;
+          if (buttonPushCounter == 11) {
+            buttonPushCounter = 1;
+            }
+
           Pressed = 1;
         }
         //Zero it so a new pressed condition can be evaluated
@@ -217,26 +229,20 @@ void button(void)
         Released_Confidence_Level = 0;
       }
     }
-    //return 0;
 }
 
-void setColor(int red, int green, int blue)
-{
-  OCR2B = 255 - red; // Set PWM rate 0 - 255 RED
-  OCR0B = 255 - green; // Set PWM rate 0 - 255 GREEN
-  OCR0A = 255 - blue; // Set PWM rate 0 - 255 BLUE
-}
 
 int main(void)
 {
-  DDRD |= 1 << PD7; // Set PB2 as output
-  DDRD &= ~(1 << PD4); // Set PB2 as input
-  PORTD |= 1 << PD4; // Set PB2 as input
+  DDRD |= 1 << PD7; // Set PD7/pin13 as output
+  DDRD &= ~(1 << PD4); // Set PD4/pin6 as input
+  PORTD |= 1 << PD4; // Set PD4/pin6 as input
 
   DDRD |= (1 << PD3); // PD3/pin5 is now an output
   DDRD |= (1 << PD5); // PD5/pin11 is now an output
-  DDRD |= (1 << PD6); // PD/pin12 is now an output
-
+  DDRD |= (1 << PD6); // PD6/pin12 is now an output
+  
+  // PWM timers for the RGB LED's
   TCCR2A =  (1 << COM2B1) | (1<<COM0A1) | (1<<WGM00) | (1 << WGM20);
   TCCR2B = (1 << CS22) | (1<<CS01);
   TCCR0A = (1<<WGM00) | (1<<COM0A1) | (1 << COM2B1) | (1 << WGM20);
@@ -254,14 +260,51 @@ int main(void)
     
   //uint8_t i;
   
-  max7219_init();
+  max7219_init(); // Start the 7xxx logic
   while (1)
   {
   button();
-  flash_led();
-  //setColor(0, 255, 0); // GREEN
-  //setColor(0, 0, 255); // BLUE
-  setColor(255, 0, 0); // RED
+  eye_display();
+  if (buttonPushCounter == 1) {
+    setColor(255, 255, 0); // YELLOW
+    }
+
+  if (buttonPushCounter == 2) {
+    setColor(255, 127, 0);  // Orange
+    }
+
+  if (buttonPushCounter == 3) {
+    setColor(255, 0, 0); // RED
+    }
+
+  if (buttonPushCounter == 4) {
+    setColor(175, 0, 175);  // Purple
+    }
+
+  if (buttonPushCounter == 5) {
+    setColor(0, 255, 255);  // Aqua
+    }
+
+  if (buttonPushCounter == 6) {
+    setColor(0, 0, 255); // BLUE
+    }
+
+  if (buttonPushCounter == 7) {
+    setColor(0, 255, 0); // GREEN
+    }
+    
+  if (buttonPushCounter == 8) {
+    setColor(255, 255, 255); // WHITE 
+    }
+
+  if (buttonPushCounter == 9) {
+    PORTD |= _BV(PORTD7); // Candle LED on
+    setColor(0, 0, 0); // OFF
+    }
+    
+  if (buttonPushCounter == 10) {
+    setColor(0, 0, 0); // OFF
+    }
   }
 }
 
