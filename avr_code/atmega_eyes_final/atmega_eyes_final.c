d9280f2329b5c3685aa8f2c3d5cc175911199f05
facelessloser <facelessloser@gmail.com>
2015-09-26 15:51:17 +0100

    cycle though animations between eyes and hearts

--- avr_code/atmega_eyes_final/atmega_eyes_final.c
+++ avr_code/atmega_eyes_final/atmega_eyes_final.c
@@ -18,6 +18,24 @@
 
 int buttonPushCounter = 1; // Keeps count of the button pushes
 
+// Timer amounts
+volatile unsigned long timerMillis1;
+long eyeSince1 = 0;
+long eyeSince2 = 1000;
+long eyeSince3 = 2000;
+long eyeSince4 = 3000;
+long eyeWaitTime = 4000;
+
+long heartSince1 = 0;
+long heartSince2 = 1000;
+long heartWaitTime = 2000;
+
+long rotateSince1 = 0;
+long rotateSince2 = 10000;
+long rotateWaitTime = 20000;
+
+int eyeRotate = 0;
+
 const __flash uint8_t eye[8] = {
         0b00000000,
         0b00111100,
@@ -48,6 +66,46 @@ const __flash uint8_t eye2[8] = {
         0b00111100,
         0b0000000};
 
+const __flash uint8_t bigHeart[8] = {
+        0b01100110,
+        0b11111111,
+        0b11111111,
+        0b11111111,
+        0b01111110,
+        0b00111100,
+        0b00011000,
+        0b00000000};
+          
+const __flash uint8_t smallHeart[8] = {
+        0b00000000,
+        0b00100100,
+        0b01111110,
+        0b01111110,
+        0b00111100,
+        0b00011000,
+        0b00000000,
+        0b00000000};
+
+//const __flash uint8_t bigHeart[8] = {
+//        0b11111111,
+//        0b10000001,
+//        0b10000001,
+//        0b10000001,
+//        0b10000001,
+//        0b10000001,
+//        0b10000001,
+//        0b11111111};
+//
+//          
+//const __flash uint8_t smallHeart[8] = {
+//        0b00000000,
+//        0b00100100,
+//        0b01111110,
+//        0b01111110,
+//        0b00111100,
+//        0b00011000,
+//        0b00000000,
+//        0b00000000};
 void setColor(int red, int green, int blue)
 {
   OCR2B = 255 - red; // Set PWM rate 0 - 255 RED
@@ -55,7 +113,7 @@ void setColor(int red, int green, int blue)
   OCR0A = 255 - blue; // Set PWM rate 0 - 255 BLUE
 }
 
-void spi_send(uint8_t data)
+void spiSend(uint8_t data)
 {
     uint8_t i;
 
@@ -71,49 +129,49 @@ void spi_send(uint8_t data)
     
 }
 
-void max7219_writec(uint8_t high_byte, uint8_t low_byte)
+void max7219Writec(uint8_t highByte, uint8_t lowByte)
 {
     CS_LOW();
-    spi_send(high_byte);
-    spi_send(low_byte);
+    spiSend(highByte);
+    spiSend(lowByte);
     CS_HIGH();
 }
 
-void max7219_clear(void)
+void max7219Clear(void)
 {
     uint8_t i;
     for (i = 0; i < 8; i++)
     {
-	max7219_writec(i+1, 0);
+	max7219Writec(i+1, 0);
     }
 }
 
-void max7219_init(void)
+void max7219Init(void)
 {
     INIT_PORT();
     // Decode mode: none
-    max7219_writec(0x09, 0);
+    max7219Writec(0x09, 0);
     // Intensity: 3 (0-15)
-    max7219_writec(0x0A, 1);
+    max7219Writec(0x0A, 1);
     // Scan limit: All "digits" (rows) on
-    max7219_writec(0x0B, 7);
+    max7219Writec(0x0B, 7);
     // Shutdown register: Display on
-    max7219_writec(0x0C, 1);
+    max7219Writec(0x0C, 1);
     // Display test: off
-    max7219_writec(0x0F, 0);
-    max7219_clear();
+    max7219Writec(0x0F, 0);
+    max7219Clear();
 }
 
 
 uint8_t display[8];
 
-void update_display(void)
+void updateDisplay(void)
 {
     uint8_t i;
 
     for (i = 0; i < 8; i++)
     {
-	max7219_writec(i+1, display[i]);
+	max7219Writec(i+1, display[i]);
     }
 }
 
@@ -125,7 +183,7 @@ void image(const __flash uint8_t im[8])
 	display[i] = im[i];
 }
 
-void set_pixel(uint8_t r, uint8_t c, uint8_t value)
+void setPixel(uint8_t r, uint8_t c, uint8_t value)
 {
     switch (value)
     {
@@ -141,67 +199,91 @@ void set_pixel(uint8_t r, uint8_t c, uint8_t value)
     }
 }
 
-// Timer amounts
-volatile unsigned long timer1_millis;
-long milliseconds_since1 = 0;
-long milliseconds_since2 = 1000;
-long milliseconds_since3 = 2000;
-long milliseconds_since4 = 3000;
-long waitTime = 4000;
 
 ISR (TIMER1_COMPA_vect)
 {
-    timer1_millis++;
+    timerMillis1++;
 }
 
 unsigned long millis()
 {
-  unsigned long millis_return;
+  unsigned long millisReturn;
 
-  millis_return = timer1_millis;
+  millisReturn = timerMillis1;
  
-  return millis_return;
+  return millisReturn;
 }
 
-void eye_display ()
+void eyeHeart ()
 {
-  if (millis() >= milliseconds_since1) {
-    image(eye); // Load image
-    update_display();
-    milliseconds_since1 += waitTime;
+  if (millis() >= rotateSince1) {
+    eyeRotate = 0;
+    rotateSince1 += rotateWaitTime;
     }
 
-  if (millis() >= milliseconds_since2) {
-    image(eye1); // Load image
-    update_display();
-    milliseconds_since2 += waitTime;
+  if (millis() >= rotateSince2) {
+    eyeRotate = 1; 
+    rotateSince2 += rotateWaitTime;
     }
+}
 
-  if (millis() >= milliseconds_since3) {
-    image(eye);
-    update_display();
-    milliseconds_since3 += waitTime;
-    }
+void eyeDisplay ()
+{
+  if (eyeRotate == 0)
+  {
+    if (millis() >= eyeSince1) {
+      image(eye); // Load image
+      updateDisplay(); // Display on led
+      eyeSince1 += eyeWaitTime;
+      }
 
-  if (millis() >= milliseconds_since4) {
-    image(eye2);
-    update_display();
-    milliseconds_since4 += waitTime;
-    }
+    if (millis() >= eyeSince2) {
+      image(eye1); // Load image
+      updateDisplay(); // Display on led
+      eyeSince2 += eyeWaitTime;
+      }
+
+    if (millis() >= eyeSince3) {
+      image(eye); // Load image
+      updateDisplay(); // Display on led
+      eyeSince3 += eyeWaitTime;
+      }
+
+    if (millis() >= eyeSince4) {
+      image(eye2); // Load image
+      updateDisplay(); // Display on led
+      eyeSince4 += eyeWaitTime;
+      }
+  }
+
+  if (eyeRotate == 1)
+  {
+    if (millis() >= heartSince1) {
+      image(bigHeart); // Load image
+      updateDisplay(); // Display on led
+      heartSince1 += heartWaitTime;
+      }
+
+    if (millis() >= heartSince2) {
+      image(smallHeart); // Load image
+      updateDisplay(); // Display on led
+      heartSince2 += heartWaitTime;
+      }
+  }
 }
  
 
 int Pressed = 0;
-int Pressed_Confidence_Level = 0; //Measure button press cofidence
-int Released_Confidence_Level = 0; //Measure button release confidence
+int pressedConfidenceLevel = 0; //Measure button press cofidence
+int releasedConfidenceLevel = 0; //Measure button release confidence
 
 void button(void)
 {
     if (bit_is_clear(PIND, 4)) // PD4
     {
-      Pressed_Confidence_Level ++; //Increase Pressed Conficence
-      Released_Confidence_Level = 0; //Reset released button confidence since there is a button press
-      if (Pressed_Confidence_Level >500) //Indicator of good button press
+      pressedConfidenceLevel ++; //Increase Pressed Conficence
+      releasedConfidenceLevel = 0; //Reset released button confidence since there is a button press
+      if (pressedConfidenceLevel >500) //Indicator of good button press
       {
         if (Pressed == 0)
         {
@@ -215,56 +297,25 @@ void button(void)
           Pressed = 1;
         }
         //Zero it so a new pressed condition can be evaluated
-        Pressed_Confidence_Level = 0;
+        pressedConfidenceLevel = 0;
       }
     }
 
     else
     {
-      Released_Confidence_Level ++; //This works just like the pressed
-      Pressed_Confidence_Level = 0; //Reset pressed button confidence since the button is released
-      if (Released_Confidence_Level >500)
+      releasedConfidenceLevel++; //This works just like the pressed
+      pressedConfidenceLevel = 0; //Reset pressed button confidence since the button is released
+      if (releasedConfidenceLevel >500)
       {
         Pressed = 0;
-        Released_Confidence_Level = 0;
+        releasedConfidenceLevel = 0;
       }
     }
 }
 
-
-int main(void)
+void colourChange(void)
 {
-  DDRD |= 1 << PD7; // Set PD7/pin13 as output
-  DDRD &= ~(1 << PD4); // Set PD4/pin6 as input
-  PORTD |= 1 << PD4; // Set PD4/pin6 as input
 
-  DDRD |= (1 << PD3); // PD3/pin5 is now an output
-  DDRD |= (1 << PD5); // PD5/pin11 is now an output
-  DDRD |= (1 << PD6); // PD6/pin12 is now an output
-  
-  // PWM timers for the RGB LED's
-  TCCR2A =  (1 << COM2B1) | (1<<COM0A1) | (1<<WGM00) | (1 << WGM20);
-  TCCR2B = (1 << CS22) | (1<<CS01);
-  TCCR0A = (1<<WGM00) | (1<<COM0A1) | (1 << COM2B1) | (1 << WGM20);
-  TCCR0B = (1<<CS01);
-
-
-  TCCR1B |= (1 << WGM12) | (1 << CS11); // CTC mode, Clock/8
- 
-  OCR1AH = (CTC_MATCH_OVERFLOW >> 8); // Load the high byte, then the low byte into the output compare
-  OCR1AL = (unsigned char) CTC_MATCH_OVERFLOW; // Added (unsigned char)
-
-  TIMSK1 |= (1 << OCIE1A); // Enable the compare match interrupt
-
-  sei(); // Now enable global interrupts
-    
-  //uint8_t i;
-  
-  max7219_init(); // Start the 7xxx logic
-  while (1)
-  {
-  button();
-  eye_display();
   if (buttonPushCounter == 1) {
     setColor(255, 255, 0); // YELLOW
     }
@@ -305,6 +356,44 @@ int main(void)
   if (buttonPushCounter == 10) {
     setColor(0, 0, 0); // OFF
     }
+}
+
+
+int main(void)
+{
+  DDRD |= 1 << PD7; // Set PD7/pin13 as output
+  DDRD &= ~(1 << PD4); // Set PD4/pin6 as input
+  PORTD |= 1 << PD4; // Set PD4/pin6 as input
+
+  DDRD |= (1 << PD3); // PD3/pin5 is now an output
+  DDRD |= (1 << PD5); // PD5/pin11 is now an output
+  DDRD |= (1 << PD6); // PD6/pin12 is now an output
+  
+  // PWM timers for the RGB LED's
+  TCCR2A =  (1 << COM2B1) | (1<<COM0A1) | (1<<WGM00) | (1 << WGM20);
+  TCCR2B = (1 << CS22) | (1<<CS01);
+  TCCR0A = (1<<WGM00) | (1<<COM0A1) | (1 << COM2B1) | (1 << WGM20);
+  TCCR0B = (1<<CS01);
+
+
+  TCCR1B |= (1 << WGM12) | (1 << CS11); // CTC mode, Clock/8
+ 
+  OCR1AH = (CTC_MATCH_OVERFLOW >> 8); // Load the high byte, then the low byte into the output compare
+  OCR1AL = (unsigned char) CTC_MATCH_OVERFLOW; // Added (unsigned char)
+
+  TIMSK1 |= (1 << OCIE1A); // Enable the compare match interrupt
+
+  sei(); // Now enable global interrupts
+    
+  //uint8_t i;
+  
+  max7219Init(); // Start the 7xxx logic
+  while (1)
+  {
+  button();
+  eyeDisplay();
+  eyeHeart();
+  colourChange();
   }
 }
 
